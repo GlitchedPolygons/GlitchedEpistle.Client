5a44dd344287bdd9ba50e9e4d4b7ee65ed8e4a80
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-01-30 00:55:12 +0100

    Added RSA keygen service to client shared code.

---
+++ GlitchedEpistle.Client/Services/Cryptography/Asymmetric/IAsymmetricKeygen.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Asymmetric
+{
+    /// <summary>
+    /// Asymmetric crypto key generator.
+    /// </summary>
+    public interface IAsymmetricKeygen
+    {
+        /// <summary>
+        /// Generates the key pair.
+        /// </summary>
+        /// <param name="outputDirectory">The output directory (where the keys should be exported into).</param>
+        /// <returns>Whether the key generation was successful or not.</returns>
+        Task<bool> GenerateKeyPair(string outputDirectory);
+    }
+}
