e67b53df7bc4864d881b61fc3f20b14aa8ea4827
Raphael Beck <glitchedpolygons@gmail.com>
2019-01-30 02:59:09 +0100

    Added custom json property name attributes to Message model (shared code) to allow easy deserializing of web API message-related responses.

--- GlitchedEpistle.Client/Models/Message.cs
+++ GlitchedEpistle.Client/Models/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Models
 {
@@ -10,32 +11,38 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Models
         /// <summary>
         /// The sender's ID.
         /// </summary>
+        [JsonProperty(PropertyName = "senderId")]
         public string SenderId { get; set; }
 
         /// <summary>
         /// The sender's display username.
         /// </summary>
+        [JsonProperty(PropertyName = "senderName")]
         public string SenderName { get; set; }
 
         /// <summary>
         /// The message's timestamp in UTC.
         /// </summary>
+        [JsonProperty(PropertyName = "utc")]
         public DateTime Timestamp { get; set; }
 
         /// <summary>
         /// The method's encrypted text.
         /// </summary>
+        [JsonProperty(PropertyName = "txt")]
         public string EncryptedText { get; set; }
 
         /// <summary>
         /// The encrypted attachment file name.
         /// </summary>
+        [JsonProperty(PropertyName = "fileName")]
         public string EncryptedAttachmentFileName { get; set; }
 
         /// <summary>
         /// Attached files are not stored inside the message body due to MongoDB's 16MB document size limit.<para> </para>
         /// Instead, a unique <see langword="string"/> is stored for downloading the attachment via GridFS.
         /// </summary>
+        [JsonProperty(PropertyName = "fileId")]
         public string EncryptedAttachmentFileId { get; set; } = null;
     }
 }
