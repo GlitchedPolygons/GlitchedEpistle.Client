4bc45ad1c32193e8533acfc7d7afe39da8bc1c60
Raphael Beck <glitchedpolygons@gmail.com>
2019-01-30 02:26:13 +0100

    Extracted various service interface definitions to shared codebase.

---
+++ GlitchedEpistle.Client/Extensions/DirectoryInfoExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Extensions
+{
+    /// <summary>
+    /// Class that holds <see cref="DirectoryInfo"/> extension methods.
+    /// </summary>
+    public static class DirectoryInfoExtensions
+    {
+        /// <summary>
+        /// Deletes the specified directory recursively,
+        /// including all of its sub-directories and files.
+        /// </summary>
+        /// <param name="dir">The directory to delete.</param>
+        public static void DeleteRecursively(this DirectoryInfo dir)
+        {
+            foreach (var file in dir.GetFiles())
+            {
+                file.Delete();
+            }
+
+            foreach (var subDir in dir.GetDirectories())
+            {
+                DeleteRecursively(subDir);
+                subDir.Delete();
+            }
+        }
+    }
+}
---
+++ GlitchedEpistle.Client/Services/Logging/ILogger.cs
@@ -0,0 +1,27 @@
+﻿
+namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Logging
+{
+    /// <summary>
+    /// Service interface for logging messages to their corresponding category's log file.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs an innocent message.
+        /// </summary>
+        /// <param name="msg">The message.</param>
+        void LogMessage(string msg);
+
+        /// <summary>
+        /// Logs a warning.
+        /// </summary>
+        /// <param name="msg">The warning.</param>
+        void LogWarning(string msg);
+
+        /// <summary>
+        /// Logs an error.
+        /// </summary>
+        /// <param name="msg">The error.</param>
+        void LogError(string msg);
+    }
+}
