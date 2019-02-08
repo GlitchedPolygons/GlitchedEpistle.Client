5cadb79005dd8cbb96921829dcd88669b1e07074
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-02-08 03:15:56 +0100

    Added IConvoProvider.cs

---
+++ GlitchedEpistle.Client/Services/Convos/IConvoProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using GlitchedPolygons.GlitchedEpistle.Client.Models;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Convos
+{
+    /// <summary>
+    /// Service interface responsible for retrieving/accessing convos.
+    /// </summary>
+    public interface IConvoProvider
+    {
+        /// <summary>
+        /// Gets all the convos currently loaded.
+        /// </summary>
+        /// <value>The convos.</value>
+        ICollection<Convo> Convos { get; }
+
+        /// <summary>
+        /// Gets the <see cref="Convo"/> with the specified identifier.
+        /// </summary>
+        /// <param name="id">The <see cref="Convo.Id"/> identifier.</param>
+        /// <returns>The matching <see cref="Convo"/> (or <c>null</c> if the convo couldn't be found).</returns>
+        Convo this[string id] { get; }
+    }
+}
