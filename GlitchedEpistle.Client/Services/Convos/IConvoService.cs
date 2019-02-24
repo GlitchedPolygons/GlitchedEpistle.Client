5554f9d0029c9dd068e58142c9ff5e36ce6a8b5a
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-02-24 23:52:14 +0100

    Added convo dtos + implemented other convo service web api request methods

--- GlitchedEpistle.Client/Services/Convos/IConvoService.cs
+++ GlitchedEpistle.Client/Services/Convos/IConvoService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using GlitchedPolygons.GlitchedEpistle.Client.Models.DTOs;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Convos
 {
@@ -8,5 +9,14 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Convos
     public interface IConvoService
     {
         Task<string> DownloadAttachment(string attachmentId, string convoId, string userId, string auth);
+        Task<string> CreateConvo(ConvoCreationDto convoDto, string userId, string auth);
+        Task<bool> DeleteConvo(string convoId, string convoPasswordHash, string userId, string auth);
+        Task<bool> PostMessage(string convoId, string convoPasswordHash, string userId, string auth, string senderName, string messageBodiesJson);
+        Task<ConvoMetadataDto> GetConvoMetadata(string convoId, string convoPasswordHash, string userId, string auth);
+        Task<string> GetConvoMessages(string convoId, string convoPasswordHash, string userId, string auth, int fromIndex = 0);
+        Task<int> IndexOf(string convoId, string convoPasswordHash, string userId, string auth, string messageId);
+        Task<bool> JoinConvo(string convoId, string convoPasswordHash, string userId, string auth);
+        Task<bool> LeaveConvo(string convoId, string convoPasswordHash, string userId, string auth);
+        Task<bool> KickUser(string convoId, string convoPasswordHash, string convoAdminId, string auth, string userIdToKick, bool permaBan);
     }
 }
