f21b94d3e498755dee53c1d63df9df0946d93544
Raphael Beck <glitchedpolygons@gmail.com>
2019-02-27 00:26:24 +0100

    Added SymmetricCryptography - needs implementation now!

---
+++ GlitchedEpistle.Client/Services/Cryptography/Symmetric/ISymmetricCryptography.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Symmetric
+{
+    public interface ISymmetricCryptography
+    {
+        Task<string> Encrypt(string plainText, string password, string salt); // TODO: wip
+        Task<string> Decrypt(string encryptedText, string password, string salt);
+    }
+}
---
+++ GlitchedEpistle.Client/Services/Cryptography/Symmetric/SymmetricCryptography.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Symmetric
+{
+    public class SymmetricCryptography : ISymmetricCryptography
+    {
+        public async Task<string> Encrypt(string plainText, string password, string salt)
+        {
+            //string encryptedText;
+            //using (var input = new MemoryStream(Encoding.UTF8.GetBytes(plainText)))
+            //using (var output = new MemoryStream())
+            //using (var rfc = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(salt), 133769))
+            //using (var aes = new AesManaged { Key = rfc.GetBytes(32) })
+            //using (var cryptoStream = new CryptoStream(output, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            //{
+            //    await input.CopyToAsync(cryptoStream);
+            //    encryptedText = Convert.ToBase64String(output.ToArray());
+            //}
+            //return encryptedText;
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> Decrypt(string encryptedText, string password, string salt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
