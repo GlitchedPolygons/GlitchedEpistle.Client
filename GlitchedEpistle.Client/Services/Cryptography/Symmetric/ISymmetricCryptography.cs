f81b25d3a68d6c7ca54eb439ba412014df617d69
Raphael Beck <glitchedpolygons@gmail.com>
2019-03-02 23:47:00 +0100

    Added EncryptWithPassword and DecryptWithPassword implementations to SymmetricCryptography service class. Seems to work, so far so good...

--- GlitchedEpistle.Client/Services/Cryptography/Symmetric/ISymmetricCryptography.cs
+++ GlitchedEpistle.Client/Services/Cryptography/Symmetric/ISymmetricCryptography.cs
@@ -1,7 +1,9 @@
 ﻿namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Symmetric
 {
     /// <summary>
-    /// Service interface for symmetrically encrypting/decrypting data (raw <c>byte[]</c> arrays).
+    /// Service interface for symmetrically encrypting/decrypting data (raw <c>byte[]</c> arrays).<para> </para>
+    /// Please keep in mind that the data you encrypt with <see cref="EncryptWithPassword(byte[],string)"/> can only be decrypted using the same password and the corresponding mirror method <see cref="DecryptWithPassword(byte[],string)"/>.<para> </para>
+    /// Likewise, data encrypted using <see cref="Encrypt"/> can only be decrypted again using <see cref="Decrypt"/> respectively.
     /// </summary>
     public interface ISymmetricCryptography
     {
@@ -14,10 +16,26 @@
         EncryptionResult Encrypt(byte[] data);
 
         /// <summary>
-        /// Decrypts the specified <see cref="EncryptionResult"/> that was obtained using <see cref="ISymmetricCryptography.Encrypt"/>.
+        /// Decrypts the specified <see cref="EncryptionResult"/> that was obtained using <see cref="ISymmetricCryptography.Encrypt(byte[])"/>.
         /// </summary>
-        /// <param name="encryptionResult">The <see cref="EncryptionResult"/> that was obtained using <see cref="ISymmetricCryptography.Encrypt"/>.</param>
+        /// <param name="encryptionResult">The <see cref="EncryptionResult"/> that was obtained using <see cref="ISymmetricCryptography.Encrypt(byte[])"/>.</param>
         /// <returns>Decrypted <c>byte[]</c> or <see langword="null"/> if decryption failed.</returns>
         byte[] Decrypt(EncryptionResult encryptionResult);
+
+        /// <summary>
+        /// Encrypts data using a password.
+        /// </summary>
+        /// <param name="data">The data to encrypt.</param>
+        /// <param name="password">The password used to derive the AES key.</param>
+        /// <returns>The encrypted data bytes.</returns>
+        byte[] EncryptWithPassword(byte[] data, string password);
+
+        /// <summary>
+        /// Decrypts data that was encrypted using <see cref="EncryptWithPassword"/>.
+        /// </summary>
+        /// <param name="encryptedData">The encrypted data.</param>
+        /// <param name="password">The password that was used to encrypt the data.</param>
+        /// <returns>The decrypted <c>byte[]</c> array.</returns>
+        byte[] DecryptWithPassword(byte[] encryptedData, string password);
     }
 }
