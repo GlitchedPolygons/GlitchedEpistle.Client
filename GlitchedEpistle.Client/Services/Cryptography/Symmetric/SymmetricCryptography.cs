37f69a0a623b15c970af2730549c64dc26e26373
GlitchedPolygons <glitchedpolygons@gmail.com>
2019-03-04 11:26:23 +0100

    Wrapped cryptographic methods into try/catch blocks to prevent exceptions being swallowed silently when calling them from Task.Run - now the caller can know something failed by checking that null was returned by the method.

--- GlitchedEpistle.Client/Services/Cryptography/Symmetric/SymmetricCryptography.cs
+++ GlitchedEpistle.Client/Services/Cryptography/Symmetric/SymmetricCryptography.cs
@@ -19,25 +19,32 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Symmetri
         /// Returns an <see cref="EncryptionResult" /> containing the encrypted <c>byte[]</c> array + the used encryption key and iv.
         /// </summary>
         /// <param name="data">The data to encrypt.</param>
-        /// <returns><see cref="EncryptionResult" /> containing the encrypted <c>byte[]</c> array + the used encryption key and iv.</returns>
+        /// <returns><see cref="EncryptionResult" /> containing the encrypted <c>byte[]</c> array + the used encryption key and iv; <c>null</c> if encryption failed.</returns>
         public EncryptionResult Encrypt(byte[] data)
         {
-            EncryptionResult result;
-            using (var aes = new AesManaged())
+            try
             {
-                aes.GenerateIV();
-                aes.GenerateKey();
-                using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                EncryptionResult result;
+                using (var aes = new AesManaged())
                 {
-                    result = new EncryptionResult
+                    aes.GenerateIV();
+                    aes.GenerateKey();
+                    using (ICryptoTransform encryptor = aes.CreateEncryptor())
                     {
-                        iv = aes.IV,
-                        key = aes.Key,
-                        encryptedData = encryptor.TransformFinalBlock(data, 0, data.Length)
-                    };
+                        result = new EncryptionResult
+                        {
+                            iv = aes.IV,
+                            key = aes.Key,
+                            encryptedData = encryptor.TransformFinalBlock(data, 0, data.Length)
+                        };
+                    }
                 }
+                return result;
             }
-            return result;
+            catch (Exception)
+            {
+                return null;
+            }
         }
         
         /// <summary>
@@ -47,17 +54,25 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Symmetri
         /// <returns>Decrypted <c>byte[]</c> or <see langword="null" /> if decryption failed.</returns>
         public byte[] Decrypt(EncryptionResult encryptionResult)
         {
-            byte[] decryptedBytes;
-            using (var aes = new AesManaged())
+            try
             {
-                aes.IV = encryptionResult.iv;
-                aes.Key = encryptionResult.key;
-                using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                byte[] decryptedBytes;
+                using (var aes = new AesManaged())
                 {
-                    decryptedBytes = decryptor.TransformFinalBlock(encryptionResult.encryptedData, 0, encryptionResult.encryptedData.Length);
+                    aes.IV = encryptionResult.iv;
+                    aes.Key = encryptionResult.key;
+                    using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                    {
+                        decryptedBytes = decryptor.TransformFinalBlock(encryptionResult.encryptedData, 0, encryptionResult.encryptedData.Length);
+                    }
                 }
+
+                return decryptedBytes;
             }
-            return decryptedBytes;
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         /// <summary>
@@ -65,26 +80,34 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Symmetri
         /// </summary>
         /// <param name="data">The data to encrypt.</param>
         /// <param name="password">The password used to derive the AES key.</param>
-        /// <returns>The encrypted data bytes.</returns>
+        /// <returns>The encrypted data <c>byte[]</c> array; <c>null</c> if encryption failed in some way.</returns>
         public byte[] EncryptWithPassword(byte[] data, string password)
         {
-            byte[] result;
-            byte[] salt = new byte[32];
-            using (var aes = new AesManaged())
-            using (var rng = new RNGCryptoServiceProvider())
+            try
             {
-                rng.GetBytes(salt);
-                using (var rfc = new Rfc2898DeriveBytes(password, salt, RFC_ITERATIONS))
+                byte[] result;
+                byte[] salt = new byte[32];
+                using (var aes = new AesManaged())
+                using (var rng = new RNGCryptoServiceProvider())
                 {
-                    aes.IV = rfc.GetBytes(16);
-                    aes.Key = rfc.GetBytes(32);
-                    using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                    rng.GetBytes(salt);
+                    using (var rfc = new Rfc2898DeriveBytes(password, salt, RFC_ITERATIONS))
                     {
-                        result = salt.Concat(encryptor.TransformFinalBlock(data, 0, data.Length)).ToArray();
+                        aes.IV = rfc.GetBytes(16);
+                        aes.Key = rfc.GetBytes(32);
+                        using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                        {
+                            result = salt.Concat(encryptor.TransformFinalBlock(data, 0, data.Length)).ToArray();
+                        }
                     }
                 }
+
+                return result;
             }
-            return result;
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         /// <summary>
@@ -92,31 +115,38 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Symmetri
         /// </summary>
         /// <param name="encryptedBytes">The encrypted data that was returned by <see cref="EncryptWithPassword(byte[],string)"/>.</param>
         /// <param name="password">The password that was used to encrypt the data.</param>
-        /// <returns>The decrypted <c>byte[]</c> array.</returns>
+        /// <returns>The decrypted <c>byte[]</c> array; <c>null</c> if decryption failed.</returns>
         public byte[] DecryptWithPassword(byte[] encryptedBytes, string password)
         {
-            byte[] decryptedBytes;
-            byte[] salt = new byte[32];
-            byte[] encr = new byte[encryptedBytes.Length - 32];
-
-            for (int i = 0; i < salt.Length; i++)
-                salt[i] = encryptedBytes[i];
-
-            for (int i = 0; i < encr.Length; i++)
-                encr[i] = encryptedBytes[i + 32];
-
-            using (var aes = new AesManaged())
-            using (var rfc = new Rfc2898DeriveBytes(password, salt, RFC_ITERATIONS))
+            try
             {
-                aes.IV = rfc.GetBytes(16);
-                aes.Key = rfc.GetBytes(32);
-                using (ICryptoTransform decryptor = aes.CreateDecryptor())
-                {
-                    decryptedBytes = decryptor.TransformFinalBlock(encr, 0, encr.Length);
-                }
-            }
+                byte[] decryptedBytes;
+                byte[] salt = new byte[32];
+                byte[] encr = new byte[encryptedBytes.Length - 32];
 
-            return decryptedBytes;
+                for (int i = 0; i < salt.Length; i++)
+                    salt[i] = encryptedBytes[i];
+
+                for (int i = 0; i < encr.Length; i++)
+                    encr[i] = encryptedBytes[i + 32];
+
+                using (var aes = new AesManaged())
+                using (var rfc = new Rfc2898DeriveBytes(password, salt, RFC_ITERATIONS))
+                {
+                    aes.IV = rfc.GetBytes(16);
+                    aes.Key = rfc.GetBytes(32);
+                    using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                    {
+                        decryptedBytes = decryptor.TransformFinalBlock(encr, 0, encr.Length);
+                    }
+                }
+
+                return decryptedBytes;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
