fa07040e0ca5a2325b662a8cae4122bba0be4f98
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-03-14 15:18:53 +0100

    xml docs + updated compiled helpfile (docs)

--- GlitchedEpistle.Client/Models/DTOs/UserCreationDto.cs
+++ GlitchedEpistle.Client/Models/DTOs/UserCreationDto.cs
@@ -23,20 +23,35 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Models.DTOs
         public string CreationSecret{ get; set; }
 
         #region Equality
-        
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>true if the current object is equal to the <paramref name="other">other</paramref> parameter; otherwise, false.</returns>
         public bool Equals(UserCreationDto other)
         {
-            return string.Equals(PasswordHash, other.PasswordHash) &&
+            return other != null && 
+                   string.Equals(PasswordHash, other.PasswordHash) &&
                    string.Equals(PublicKeyXml, other.PublicKeyXml) &&
                    string.Equals(CreationSecret, other.CreationSecret);
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.</returns>
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
             return obj is UserCreationDto other && Equals(other);
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
         public override int GetHashCode()
         {
             unchecked
