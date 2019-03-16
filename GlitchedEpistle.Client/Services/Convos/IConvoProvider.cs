47144402ee4b2b05e17d91e048b7be23f37f2f91
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-03-16 21:56:53 +0100

    Added more functionality to IConvoProvider.

--- GlitchedEpistle.Client/Services/Convos/IConvoProvider.cs
+++ GlitchedEpistle.Client/Services/Convos/IConvoProvider.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using GlitchedPolygons.GlitchedEpistle.Client.Models;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Convos
 {
     /// <summary>
-    /// Service interface responsible for retrieving/accessing convos (local).
+    /// Service interface responsible for accessing convos (local).
     /// </summary>
     public interface IConvoProvider
     {
@@ -20,5 +21,27 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Convos
         /// <param name="id">The <see cref="Convo.Id"/> identifier.</param>
         /// <returns>The matching <see cref="Convo"/> (or <c>null</c> if the convo couldn't be found).</returns>
         Convo this[string id] { get; }
+
+        /// <summary>
+        /// Saves all the currently loaded <see cref="Convo"/>s in memory out to disk (or somewhere persistent; depends on the implementation).
+        /// </summary>
+        void Save();
+
+        /// <summary>
+        /// Loads all <see cref="Convo"/>s available on the persistent storage medium (usually local hard drive)
+        /// into the <see cref="Convos"/> collection (the existing collection is overwritten!).
+        /// </summary>
+        void Load();
+
+        /// <summary>
+        /// Occurs when the <see cref="IConvoProvider"/> has finished loading/refreshing <see cref="Convo"/>s into the <see cref="Convos"/> collection.
+        /// </summary>
+        event EventHandler Loaded;
+
+        /// <summary>
+        /// Shall be raised when the <see cref="IConvoProvider"/> has finished
+        /// saving the current state of the <see cref="Convos"/> out to persistent memory.
+        /// </summary>
+        event EventHandler Saved;
     }
 }
