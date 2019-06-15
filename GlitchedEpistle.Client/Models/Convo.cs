a4a81f677ff13b2bbb795b2d7b7ea00a2fa0e52f
Raphael Beck <glitchedpolygons@gmail.com>
2019-06-15 03:11:06 +0200

    Minor cleanup

--- GlitchedEpistle.Client/Models/Convo.cs
+++ GlitchedEpistle.Client/Models/Convo.cs
@@ -83,7 +83,7 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Models
         /// Determines whether this <see cref="Convo"/> is expired.
         /// </summary>
         /// <returns><c>true</c> if the <see cref="Convo"/> is expired; otherwise, <c>false</c>.</returns>
-        public bool IsExpired => DateTime.UtcNow > ExpirationUTC;
+        public bool IsExpired() => DateTime.UtcNow > ExpirationUTC;
 
         /// <summary>
         /// Gets all of the <see cref="Convo"/>'s participants (their ids) comma-separated;
