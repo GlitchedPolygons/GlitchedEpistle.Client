e6b10335df676475463ad498ecd647edf5b684ca
Raphael Beck <glitchedpolygons@gmail.com>
2019-06-26 01:30:48 +0200

    Ditched domain model's  and  properties because they are accessed respectively through their repository service interfaces.

--- GlitchedEpistle.Client/Services/Convos/IConvoPasswordProvider.cs
+++ GlitchedEpistle.Client/Services/Convos/IConvoPasswordProvider.cs
@@ -11,14 +11,14 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Convos
         /// Gets a conversation's password SHA512 from the session's password provider.<para> </para>
         /// Returns <c>null</c> if the user has never accessed the convo during the session.
         /// </summary>
-        /// <param name="convoId">The convo identifier.</param>
+        /// <param name="convoId">The <see cref="Convo"/> identifier.</param>
         /// <returns>The convo's password SHA512 <c>string</c>; <c>null</c> if the password was</returns>
         string GetPasswordSHA512(string convoId);
 
         /// <summary>
         /// Saves a convo's password SHA512 for the current app session for easy access.
         /// </summary>
-        /// <param name="convoId">The convo identifier.</param>
+        /// <param name="convoId">The <see cref="Convo"/> identifier.</param>
         /// <param name="passwordSHA512">The password's SHA512.</param>
         void SetPasswordSHA512(string convoId, string passwordSHA512);
 
