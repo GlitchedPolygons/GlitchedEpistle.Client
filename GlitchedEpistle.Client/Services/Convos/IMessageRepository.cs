dabbfc47ea698105540672210331237a24eb6858
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-06-25 04:08:02 +0200

    sqlite timestamps now stored as unix time (milliseconds since 1970-01-01 00:00:00.000Z)

---
+++ GlitchedEpistle.Client/Services/Convos/IMessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+using GlitchedPolygons.RepositoryPattern;
+using GlitchedPolygons.GlitchedEpistle.Client.Models;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Convos
+{
+    /// <summary>
+    /// Message repository.
+    /// </summary>
+    public interface IMessageRepository : IRepository<Message, string>
+    {
+        /// <summary>
+        /// Gets the <see cref="Message.Id"/> from the most recent <see cref="Message"/> in the repository.
+        /// </summary>
+        Task<string> GetLastMessageId();
+    }
+}
