839b7f6440985eaa99d87b706a9952643b34b832
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-08-02 23:34:25 +0200

    Added basic KeyExchangeUtility tests for at least ensuring equality of what you put in and retrieve back out when manipulating keys...

--- GlitchedEpistle.Client.Tests/KeyExchangeUtilityTests.cs
+++ GlitchedEpistle.Client.Tests/KeyExchangeUtilityTests.cs
@@ -1,13 +1,36 @@
+using System.IO;
+
+using GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Symmetric;
+using GlitchedPolygons.GlitchedEpistle.Client.Utilities;
+using GlitchedPolygons.Services.CompressionUtility;
+
 using Xunit;
 
 namespace GlitchedEpistle.Client.Tests
 {
     public class KeyExchangeUtilityTests
     {
+        private readonly ICompressionUtility gzip = new GZipUtility();
+        private readonly ISymmetricCryptography crypto = new SymmetricCryptography();
+
+        private readonly string privateKeyPem = File.ReadAllText("test.private.rsa");
+        private readonly string publicTestKeyPem = File.ReadAllText("test.public.rsa");
+
         [Fact]
-        public void Test1()
+        public void KeyExchangeUtility_Compress_Decompress_IdenticalAfterwards()
         {
-            Assert.True(1 + 1 == 2);
+            string compressedKey = KeyExchangeUtility.CompressPublicKey(publicTestKeyPem);
+            string decompressedKey = KeyExchangeUtility.DecompressPublicKey(compressedKey);
+            Assert.Equal(publicTestKeyPem, decompressedKey);
+        }
+
+        [Fact]
+        public void KeyExchangeUtility_EncryptAndCompress_DecompressAndDecrypt_IdenticalAfterwards()
+        {
+            const string TEST_PW = "test.@#°§çUserPassword$$$___69420  \r\n847KWdHfhoöüä!!]][}$äö\"";
+            string i = KeyExchangeUtility.EncryptAndCompressPrivateKey(privateKeyPem, TEST_PW);
+            string o = KeyExchangeUtility.DecompressAndDecryptPrivateKey(i, TEST_PW);
+            Assert.Equal(privateKeyPem, o);
         }
     }
 }
