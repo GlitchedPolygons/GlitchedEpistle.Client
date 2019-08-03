53afb992580cd746f85f2595bdb5cbb0390af779
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-08-03 02:09:14 +0200

    Added .circleci/config.yml, ready for CI. Also added more unit tests.

---
+++ GlitchedEpistle.Client.Tests/AsymmetricCryptographyRSATests.cs
@@ -0,0 +1,102 @@
+using Xunit;
+using System;
+using System.IO;
+using GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Asymmetric;
+
+namespace GlitchedEpistle.Client.Tests
+{
+    public class AsymmetricCryptographyRSATests
+    {
+        private readonly IAsymmetricCryptographyRSA crypto = new AsymmetricCryptographyRSA();
+
+        private readonly string text = Guid.NewGuid().ToString("D");
+        private readonly string privateKeyPem = File.ReadAllText("TestData/test.private.rsa");
+        private readonly string publicTestKeyPem = File.ReadAllText("TestData/test.public.rsa");
+        
+        [Fact]
+        public void AsymmetricCryptography_EncryptString_DecryptString_IdenticalAfterwards()
+        {
+            string encr = crypto.Encrypt(text, publicTestKeyPem);
+            string decr = crypto.Decrypt(encr, privateKeyPem);
+            Assert.Equal(decr, text);
+        }
+        
+        [Fact]
+        public void AsymmetricCryptography_EncryptStringUsingPrivateKey_DecryptString_IdenticalAfterwards_ShouldAlsoWork()
+        {
+            string encr = crypto.Encrypt(text, privateKeyPem);
+            string decr = crypto.Decrypt(encr, privateKeyPem);
+            Assert.Equal(decr, text);
+        }
+        
+        [Fact]
+        public void AsymmetricCryptography_EncryptString_NotIdenticalWithOriginal()
+        {
+            string encr = crypto.Encrypt(text, publicTestKeyPem);
+            Assert.NotEqual(encr, text);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void AsymmetricCryptography_EncryptNullOrEmptyString_ReturnsEmptyString(string s)
+        {
+            string encr = crypto.Encrypt(s, publicTestKeyPem);
+            Assert.Empty(encr);
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void AsymmetricCryptography_EncryptStringWithNullOrEmptyKey_ReturnsEmptyString(string s)
+        {
+            string encr = crypto.Encrypt(text, s);
+            Assert.Empty(encr);
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void AsymmetricCryptography_DecryptNullOrEmptyString_ReturnsEmptyString(string s)
+        {
+            string decr = crypto.Decrypt(s, publicTestKeyPem);
+            Assert.Empty(decr);
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void AsymmetricCryptography_DecryptStringWithNullOrEmptyKey_ReturnsEmptyString(string s)
+        {
+            string decr = crypto.Decrypt(text, s);
+            Assert.Empty(decr);
+        }
+        
+        [Fact]
+        public void AsymmetricCryptography_EncryptString_DecryptStringUsingPublicKey_ReturnsNull()
+        {
+            string encr = crypto.Encrypt(text, publicTestKeyPem);
+            string decr = crypto.Decrypt(encr, publicTestKeyPem);
+            Assert.NotEqual(text,decr);
+            Assert.Null(decr);
+        }
+        
+        [Fact]
+        public void AsymmetricCryptography_EncryptString_DecryptUsingGarbageString_ReturnsNull()
+        {
+            string encr = crypto.Encrypt(text, publicTestKeyPem);
+            string decr = crypto.Decrypt(encr, "LOL");
+            Assert.NotEqual(text,decr);
+            Assert.Null(decr);
+        }
+        
+        [Fact]
+        public void AsymmetricCryptography_EncryptStringUsingInvalidGarbageString_DecryptionFails_ReturnsNull()
+        {
+            string encr = crypto.Encrypt(text, "LOL");
+            string decr = crypto.Decrypt(encr, publicTestKeyPem);
+            Assert.NotEqual(text,decr);
+            Assert.Null(decr);
+        }
+    }
+}
--- GlitchedEpistle.Client/Services/Cryptography/Asymmetric/AsymmetricCryptographyRSA.cs
+++ GlitchedEpistle.Client/Services/Cryptography/Asymmetric/AsymmetricCryptographyRSA.cs
@@ -1,5 +1,4 @@
-﻿#region
-using System;
+﻿using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
@@ -11,7 +10,6 @@ using Org.BouncyCastle.Crypto.Encodings;
 using Org.BouncyCastle.Crypto.Parameters;
 
 using GlitchedPolygons.GlitchedEpistle.Client.Extensions;
-#endregion
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Asymmetric
 {
@@ -26,15 +24,21 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Asymmetr
         /// </summary>
         /// <param name="text">The plain text to encrypt.</param>
         /// <param name="publicKeyPem">The public RSA key for encryption (PEM-formatted <c>string</c>).</param>
-        /// <returns>The encrypted <c>string</c>; <c>null</c> if the passed key or plain text argument was <c>null</c> or empty.</returns>
+        /// <returns>The encrypted <c>string</c>; <c>string.Empty</c> if the passed key or plain text argument was <c>null</c> or empty.</returns>
         public string Encrypt(string text, string publicKeyPem)
         {
             if (text.NullOrEmpty() || publicKeyPem.NullOrEmpty())
             {
+                return string.Empty;
+            }
+            try
+            {
+                return Convert.ToBase64String(Encrypt(Encoding.UTF8.GetBytes(text), publicKeyPem));
+            }
+            catch (Exception)
+            {
                 return null;
             }
-
-            return Convert.ToBase64String(Encrypt(Encoding.UTF8.GetBytes(text), publicKeyPem));
         }
 
         /// <summary>
@@ -47,48 +51,61 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Asymmetr
         {
             if (encryptedText.NullOrEmpty() || privateKeyPem.NullOrEmpty())
             {
+                return string.Empty;
+            }
+            try
+            {
+                return Encoding.UTF8.GetString(Decrypt(Convert.FromBase64String(encryptedText), privateKeyPem));
+            }
+            catch (Exception)
+            {
                 return null;
             }
-
-            return Encoding.UTF8.GetString(Decrypt(Convert.FromBase64String(encryptedText), privateKeyPem));
         }
 
         /// <summary>
-        /// Encrypts the specified bytes using the provided RSA public key, which needs to be a PEM-formatted <c>string</c>..
+        /// Encrypts the specified bytes using the provided RSA public key, which needs to be a PEM-formatted <c>string</c>.
         /// </summary>
         /// <param name="data">The data (<c>byte[]</c> array) to encrypt.</param>
         /// <param name="publicKeyPem">The public key (PEM-formatted <c>string</c>) to use for encryption.</param>
-        /// <returns>The encrypted bytes (<c>System.Byte[]</c>); <c>null</c> if the passed data or key argument was <c>null</c> or empty.</returns>
+        /// <returns>The encrypted bytes (<c>System.Byte[]</c>); <c>Array.Empty&lt;byte&gt;()</c> if the passed data or key argument was <c>null</c> or empty; <c>null</c> if encryption failed.</returns>
         public byte[] Encrypt(byte[] data, string publicKeyPem)
         {
             if (data is null || data.Length == 0 || publicKeyPem.NullOrEmpty())
             {
+                return Array.Empty<byte>();
+            }
+
+            try
+            {
+                RsaKeyParameters keys;
+                using (var stringReader = new StringReader(publicKeyPem))
+                {
+                    var pemReader = new PemReader(stringReader);
+                    keys = (RsaKeyParameters) pemReader.ReadObject();
+                }
+
+                // PKCS1 OAEP paddings
+                OaepEncoding eng = new OaepEncoding(new RsaEngine());
+                eng.Init(true, keys);
+
+                int length = data.Length;
+                int blockSize = eng.GetInputBlockSize();
+
+                List<byte> encryptedBytes = new List<byte>(length);
+
+                for (int chunkPosition = 0; chunkPosition < length; chunkPosition += blockSize)
+                {
+                    int chunkSize = Math.Min(blockSize, length - chunkPosition);
+                    encryptedBytes.AddRange(eng.ProcessBlock(data, chunkPosition, chunkSize));
+                }
+
+                return encryptedBytes.ToArray();
+            }
+            catch (Exception)
+            {
                 return null;
             }
-
-            RsaKeyParameters keys;
-            using (var stringReader = new StringReader(publicKeyPem))
-            {
-                var pemReader = new PemReader(stringReader);
-                keys = (RsaKeyParameters)pemReader.ReadObject();
-            }
-
-            // PKCS1 OAEP paddings
-            OaepEncoding eng = new OaepEncoding(new RsaEngine());
-            eng.Init(true, keys);
-
-            int length = data.Length;
-            int blockSize = eng.GetInputBlockSize();
-
-            List<byte> encryptedBytes = new List<byte>(length);
-
-            for (int chunkPosition = 0; chunkPosition < length; chunkPosition += blockSize)
-            {
-                int chunkSize = Math.Min(blockSize, length - chunkPosition);
-                encryptedBytes.AddRange(eng.ProcessBlock(data, chunkPosition, chunkSize));
-            }
-
-            return encryptedBytes.ToArray();
         }
 
         /// <summary>
@@ -96,37 +113,44 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Services.Cryptography.Asymmetr
         /// </summary>
         /// <param name="encryptedData">The encrypted data bytes (<c>byte[]</c>).</param>
         /// <param name="privateKeyPem">The private RSA key to use for decryption (PEM-formatted <c>string</c>).</param>
-        /// <returns>Decrypted bytes (System.Byte[]) if successful; <c>null</c> if the passed data or key argument was <c>null</c> or empty.</returns>
+        /// <returns>Decrypted bytes (System.Byte[]) if successful; an empty <c>byte[]</c> array if the passed data or key argument was <c>null</c> or empty; <c>null</c> if decryption failed.</returns>
         public byte[] Decrypt(byte[] encryptedData, string privateKeyPem)
         {
             if (encryptedData is null || encryptedData.Length == 0 || privateKeyPem.NullOrEmpty())
             {
+                return Array.Empty<byte>();
+            }
+
+            try
+            {
+                AsymmetricCipherKeyPair keys;
+                using (var stringReader = new StringReader(privateKeyPem))
+                {
+                    var pemReader = new PemReader(stringReader);
+                    keys = (AsymmetricCipherKeyPair) pemReader.ReadObject();
+                }
+
+                // PKCS1 OAEP paddings
+                OaepEncoding eng = new OaepEncoding(new RsaEngine());
+                eng.Init(false, keys.Private);
+
+                int length = encryptedData.Length;
+                int blockSize = eng.GetInputBlockSize();
+
+                List<byte> decryptedBytes = new List<byte>(length);
+
+                for (int chunkPosition = 0; chunkPosition < length; chunkPosition += blockSize)
+                {
+                    int chunkSize = Math.Min(blockSize, length - chunkPosition);
+                    decryptedBytes.AddRange(eng.ProcessBlock(encryptedData, chunkPosition, chunkSize));
+                }
+
+                return decryptedBytes.ToArray();
+            }
+            catch (Exception)
+            {
                 return null;
             }
-
-            AsymmetricCipherKeyPair keys;
-            using (var stringReader = new StringReader(privateKeyPem))
-            {
-                var pemReader = new PemReader(stringReader);
-                keys = (AsymmetricCipherKeyPair)pemReader.ReadObject();
-            }
-
-            // PKCS1 OAEP paddings
-            OaepEncoding eng = new OaepEncoding(new RsaEngine());
-            eng.Init(false, keys.Private);
-
-            int length = encryptedData.Length;
-            int blockSize = eng.GetInputBlockSize();
-
-            List<byte> decryptedBytes = new List<byte>(length);
-
-            for (int chunkPosition = 0; chunkPosition < length; chunkPosition += blockSize)
-            {
-                int chunkSize = Math.Min(blockSize, length - chunkPosition);
-                decryptedBytes.AddRange(eng.ProcessBlock(encryptedData, chunkPosition, chunkSize));
-            }
-
-            return decryptedBytes.ToArray();
         }
     }
 }
